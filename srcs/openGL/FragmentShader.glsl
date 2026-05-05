217c77df8b27888b28a6549c0dd466ff56c7c199
LSV310 <anicet.gruet@gmail.com>
2026-05-05 12:35:54 +0200

    Added comments to color rectangle back

--- srcs/openGL/FragmentShader.glsl
+++ srcs/openGL/FragmentShader.glsl
@@ -10,9 +10,9 @@ uniform float smoothOpacity;
 uniform float imgOpacity;
 uniform sampler2D tex;
 
-vec3 hsvToRgb(float h, float v) {
+vec3 hsvToRgb(float h, float s, float v) {
 	vec3 rgb = clamp(abs(mod(h * 6.0 + vec3(0.0, 4.0, 2.0), 6.0) - 3.0) - 1.0, 0.0, 1.0);
-	return v * mix(vec3(1.0), rgb, 0.0);
+	return v * mix(vec3(1.0), rgb, s);
 }
 
 void main()
@@ -21,8 +21,9 @@ void main()
 
 	if (coloredOpacity != 0.0f) {
 		float hue = mod(float(gl_PrimitiveID) * 0.618033988749895, 1.0);
+		float sat = 0.0; // 0.30 + mod(float(gl_PrimitiveID) * 0.211, 0.20);
 		float val = 0.15 + mod(float(gl_PrimitiveID) * 0.900, 0.55);
-		vec3 triangleColor = hsvToRgb(hue, val) * coloredOpacity;
+		vec3 triangleColor = hsvToRgb(hue, sat, val) * coloredOpacity;
 		FragColor.x += triangleColor.x;
 		FragColor.y += triangleColor.y;
 		FragColor.z += triangleColor.z;
