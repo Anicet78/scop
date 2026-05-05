d4801da28d369d1fb20b292bf1e3992aa01af04a
LSV310 <anicet.gruet@gmail.com>
2026-05-05 12:16:01 +0200

    Added gray sides

--- srcs/openGL/FragmentShader.glsl
+++ srcs/openGL/FragmentShader.glsl
@@ -10,9 +10,9 @@ uniform float smoothOpacity;
 uniform float imgOpacity;
 uniform sampler2D tex;
 
-vec3 hsvToRgb(float h, float s, float v) {
+vec3 hsvToRgb(float h, float v) {
 	vec3 rgb = clamp(abs(mod(h * 6.0 + vec3(0.0, 4.0, 2.0), 6.0) - 3.0) - 1.0, 0.0, 1.0);
-	return v * mix(vec3(1.0), rgb, s);
+	return v * mix(vec3(1.0), rgb, 0.0);
 }
 
 void main()
@@ -21,9 +21,8 @@ void main()
 
 	if (coloredOpacity != 0.0f) {
 		float hue = mod(float(gl_PrimitiveID) * 0.618033988749895, 1.0);
-		float sat = 0.30 + mod(float(gl_PrimitiveID) * 0.211, 0.20);
-		float val = 0.97 + mod(float(gl_PrimitiveID) * 0.137, 0.15);
-		vec3 triangleColor = hsvToRgb(hue, sat, val) * coloredOpacity;
+		float val = 0.15 + mod(float(gl_PrimitiveID) * 0.900, 0.55);
+		vec3 triangleColor = hsvToRgb(hue, val) * coloredOpacity;
 		FragColor.x += triangleColor.x;
 		FragColor.y += triangleColor.y;
 		FragColor.z += triangleColor.z;
